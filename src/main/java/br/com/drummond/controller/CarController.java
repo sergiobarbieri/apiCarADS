af742d7705152b4a82a502f0489b3a1b3739df58
aluno <aluno@Lab2Professor.labtatuape.drummond.com.br>
2019-04-10 21:58:03 -0300

    MVC controller paginas html

---
+++ src/main/java/br/com/drummond/controller/CarController.java
@@ -0,0 +1,16 @@
+package br.com.drummond.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class CarController {
+	
+	@RequestMapping(value = "/carView", method = RequestMethod.GET)
+	public String getCarView() {
+		     
+		   return "carView";
+	}
+
+}
