e47d67e6a71af3abfb71a8681b22d8013aff852f
aluno <aluno@Lab2Professor.labtatuape.drummond.com.br>
2019-04-17 20:12:31 -0300

    Script inicial para carga DB ao iniciar o projeto.

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+insert into car (car_id, model, year)
+values (1, 'Ford Focus', 2017);
+insert into car (car_id, model, year)
+values (2, 'Outlander', 2010);
+insert into car (car_id, model, year)
+values (3, 'VW Up!', 2017);
