b4fdac832bb66335bd86d66275cb4a619a5cf441
aluno <aluno@Lab2Professor.labtatuape.drummond.com.br>
2019-05-29 21:16:34 -0300

    tratamento erro update e exemplo generate PK

--- src/main/java/br/com/drummond/domain/Car.java
+++ src/main/java/br/com/drummond/domain/Car.java
@@ -2,12 +2,17 @@ package br.com.drummond.domain;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
 public class Car {
 
-	@Id
+	/*
+	 * @GeneratedValue(strategy = GenerationType.IDENTITY)
+	 * @GeneratedValue(strategy = GenerationType.AUTO)
+	 */
+	@Id	
 	private Integer carId;
 	private String model;
 	private Integer year;
